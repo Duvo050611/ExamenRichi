f5a78ba4ed50341322e6bee8fd71ff6e7f7226f3
oduarte220 <oduarte220@gmail.com>
2025-02-22 11:41:16 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [pets, setPets] = useState([]);
+  const [loading, setLoading] = useState(false); // Estado de carga
+  const [error, setError] = useState(""); // Estado de error
+
+  // Consumiendo la API con fetch
+  useEffect(() => {
+    setLoading(true); // Indicamos que la carga está en progreso
+    fetch("http://3.128.206.46/api/pets")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error en la respuesta de la API");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPets(data); // Guardamos los datos obtenidos
+        setLoading(false); // Indicamos que la carga ha terminado
+      })
+      .catch((error) => {
+        setError("Hubo un error al obtener los datos: " + error.message);
+        setLoading(false); // Indicamos que la carga ha terminado incluso si hubo error
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gestión de Mascotas</h1>
+
+      {/* Mostrar mensaje de carga */}
+      {loading && <p>Cargando...</p>}
+
+      {/* Mostrar error si ocurre */}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {/* Mostrar las mascotas */}
+      <ul>
+        {pets.map((pet, index) => (
+          <li key={index}>{`${pet.name} - ${pet.raza} - ${pet.edad} años`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
